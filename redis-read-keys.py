d93ea30d1c0895b458ab32b0ecd440273553a971
Terry Bates <terryjbates@gmail.com>
2013-05-12 15:29:10 -0400

    Added more comments describing logic in main().

--- redis-read-keys.py
+++ redis-read-keys.py
@@ -2,14 +2,19 @@
 import sys
 import redis 
 
-# Connect to redis
+# Connect to Redis
 r = redis.Redis(host='localhost', port=6379, db=0)
 
 def main():
+    # Loop indefinitely.
     while True:
+        # Read input from STDIN, one line at a time. Strip off trailing chars.
         input = sys.stdin.readline().strip()
+        # Try to find a matching key in Redis
         try:
+            # Prefix the input with 'foomap.' 
             input = 'foomap:' + input
+            # Output the found value for the key
             print r.get(input)
         except:
             return None
